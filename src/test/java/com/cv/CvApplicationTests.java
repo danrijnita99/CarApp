2ef85fce18b53163aefec2f10793f7dba985d697
dan <danrijnita09@gmail.com>
2021-06-10 18:28:01 +0300

    modified first junit test

--- src/test/java/com/cv/CvApplicationTests.java
+++ src/test/java/com/cv/CvApplicationTests.java
@@ -1,13 +1,17 @@
 package com.cv;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class CvApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void firstTest() {
+		System.out.println("Junit works!");
+		Assertions.assertTrue(true);
 	}
-
 }
